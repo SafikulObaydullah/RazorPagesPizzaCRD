24ce6205ad1d840ed5cceb21a14a7afb059b6c7a
obaydullah <obaydullah599@gmail.com>
2022-06-12 21:10:56 +0600

    CRD for RazorPizza

---
+++ RazorPagesPizza/RazorPagesPizza/Pizzza.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza
+{
+    public class Pizzza : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
